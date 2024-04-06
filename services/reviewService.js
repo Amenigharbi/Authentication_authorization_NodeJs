090dd9cf94f6385b48eb02fd78c483df302b3640
Ameni <103783268+Amenigharbi@users.noreply.github.com>
2024-04-06 21:22:07 +0100

    Update reviewService.js

--- services/reviewService.js
+++ services/reviewService.js
@@ -3,7 +3,8 @@ const Review = require('../models/reviewModel');
 
 // Nested route
 // GET /api/v1/products/:productId/reviews
-exports.createFilterObj = (req, res, next) => {
+exports.createFilterObj = (req, res, next) => 
+{
   let filterObject = {};
   if (req.params.productId) filterObject = { product: req.params.productId };
   req.filterObj = filterObject;
